afccdced40cbb89a78cd4daf6d9e2d2fdfe2b6f4
Miria Santos <miriaandressa1@gmail.com>
2021-05-06 14:19:21 -0300

    Get e Post de temas implementados

--- forumDasMana/src/app/tema/tema.component.ts
+++ forumDasMana/src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { tema } from '../model/tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,8 +11,12 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: tema = new tema()
+  listaTema: tema[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -18,6 +24,23 @@ export class TemaComponent implements OnInit {
       alert('Sua sessão expirou, faça login novamente!')
       this.router.navigate(['/login'])
     }
+
+    this.findAllTemas()
+  }
+
+  findAllTemas() {
+    this.temaService.getAllTema().subscribe((resp: tema[]) => {
+      this.listaTema = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: tema) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new tema()
+    })
   }
 
 }
