95a3d48f1b31dfa229394b74ff3050686d5a1e5f
Miria Santos <miriaandressa1@gmail.com>
2021-05-14 08:29:57 -0300

    Últimos ajustes

--- forumDasMana/src/app/tema/tema.component.ts
+++ forumDasMana/src/app/tema/tema.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { tema } from '../model/tema';
+import { AlertasService } from '../service/alertas.service';
 import { TemaService } from '../service/tema.service';
 
 @Component({
@@ -16,13 +17,19 @@ export class TemaComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private alerta: AlertasService
   ) { }
 
   ngOnInit() {
     if(environment.token == '') {
-      alert('Sua sessão expirou, faça login novamente!')
+      this.alerta.showAlertDanger('Sua sessão expirou, faça login novamente!')
       this.router.navigate(['/login'])
+    }
+
+    if(environment.tipo == 'adm') {
+      this.alerta.showAlertInfo('Você precisa ser administrador para acessar esse conteúdo!')
+      this.router.navigate(['/inicio']) 
     }
 
     this.findAllTemas()
@@ -37,7 +44,7 @@ export class TemaComponent implements OnInit {
   cadastrar(){
     this.temaService.postTema(this.tema).subscribe((resp: tema) => {
       this.tema = resp
-      alert('Tema cadastrado com sucesso!')
+      this.alerta.showAlertSuccess('Tema cadastrado com sucesso!')
       this.findAllTemas()
       this.tema = new tema()
     })
